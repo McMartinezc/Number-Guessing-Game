1a486fb869624745b7672ea51629fb2d18d4db54
McMartinezc <psyence_mc@hotmail.com>
2023-10-25 11:32:11 +0200

    ok

--- app.js
+++ app.js
@@ -2,14 +2,42 @@
 
 // Variables de estado iniciales. Puede que falta alguna...
 
-let estoyJugando = true;
-let numeroCorrecto = Math.floor(Math.random() * 100);
-let numIntentos = 10;
+let isPlaying = true;
+let correctNumber = Math.floor(Math.random() * 100) + 1;
+let remainingAttempts = 10;
 
 // Actualizar el innerHTML del nodo adecuado con el número de intentos iniciales
 
-document.querySelector("form").addEventListener("submit", (e) => {
-  e.preventDefault(); // Impedimos que el formulario haga un petición GET
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault(); // Impedimos que el formulario haga un petición GET
 
   // Comprobar si el número que ha puesto el usuario es mayor o menor que el numeroCorrecto. Tomar decisiones
+
+  if (isPlaying) {
+    let userValueInput = +(document.querySelector("#guessField").value);
+    const info = document.querySelector(".lowOrHi");
+    const numGuesses = document.querySelector(".guesses");
+
+    if (userValueInput > correctNumber) {
+      info.textContent = `Too High! Try again.`;
+    } else if (userValueInput < correctNumber) {
+      info.textContent = `Too low! Try again.`;
+    } else {
+      info.textContent = `Correct number!`;
+      isPlaying = false; 
+    }
+
+    numGuesses.textContent = userValueInput;
+    remainingAttempts--;
+
+    if (remainingAttempts <= 0 && isPlaying) {
+      info.textContent = `¡You have exhausted the number of attempts! The correct number is ${correctNumber}.`;
+      isPlaying = false;
+    }
+
+    document.querySelector(".lastResult").textContent = remainingAttempts;
+
+  } else {
+    info.textContent = `End Game!`;
+  }
 });
